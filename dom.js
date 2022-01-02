b70c20c31e9c493e1e6912665d497325e3b94a2c
aakriti33 <83652313+aakrityy@users.noreply.github.com>
2022-01-02 22:52:41 +0545

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,9 @@
+ const burger=document.querySelector('.burger');
+ const mylinklist=document.querySelector('.mylinklist');
+ const myright=documemt.querySelector('.right-nav');
+burger.addEventListener('click',show);
+function show()
+{
+    mylinklist.style.display="block";
+}
+
