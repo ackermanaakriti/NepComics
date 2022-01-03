691bf2803afb5931986e70b9a717f94cac4e02e3
aakriti33 <83652313+aakrityy@users.noreply.github.com>
2022-01-03 21:20:35 +0545

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,9 +1,24 @@
+  
+ console.log("hello world");
  const burger=document.querySelector('.burger');
  const mylinklist=document.querySelector('.mylinklist');
- const myright=documemt.querySelector('.right-nav');
+ const myright=document.querySelector('.right-nav');
 burger.addEventListener('click',show);
 function show()
 {
-    mylinklist.style.display="block";
-}
+    // mylinklist.style.display="block";
+    // myright.style.display='block';
+    mylinklist.classList.toggle('show-repo');
+    myright.classList.toggle('show-repo');
+};
+$(document).ready(function()
+{
+    $('.search-btn').click(function()
+    {
+    $('.newpage').load('signup.html')
+    });
+});
+
+
+
 
